a81c575e3436f50006f0715cfa259f4bc577a99f
Penta305 <dylan.janky@gmail.com>
2018-04-12 12:20:13 +1000

    Added comments

--- src/DiscoveryController.cs
+++ src/DiscoveryController.cs
@@ -3,10 +3,18 @@ using SwinGameSDK;
 using static SwinGameSDK.SwinGame; // requires mcs version 4+, 
 // using SwinGameSDK.SwinGame; // requires mcs version 4+, 
 
+// The DiscoveryController controls the battle phase.
+
 namespace Battleship
 {
     public class DiscoveryController
     {
+
+        // Handles the user input during the discovery phase.
+
+        // Escape opens the game menu. Clicking the mouse will launch
+        // an attack at that location.
+
         public void HandleDiscoveryInput()
         {
             if (SwinGame.KeyTyped(KeyCode.EscapeKey))
@@ -20,10 +28,15 @@ namespace Battleship
             }
         }
 
+        // Launches an attack at the current mouse position.
+
         private void DoAttack()
         {
             Point2D mouse;
             mouse = SwinGame.MousePosition();
+
+            // Calculate the row and column selected.
+
             int row, col;
             row = Convert.ToInt32(Math.Floor((mouse.Y - FIELD_TOP) / (CELL_HEIGHT + CELL_GAP)));
             col = Convert.ToInt32(Math.Floor((mouse.X - FIELD_LEFT) / (CELL_WIDTH + CELL_GAP)));
@@ -35,6 +48,8 @@ namespace Battleship
                 }
             }
         }
+
+        // Draws the game field and interface during the attack phase.
 
         public void DrawDiscovery()
         {
@@ -58,10 +73,5 @@ namespace Battleship
             SwinGame.DrawText(HumanPlayer.Missed.ToString(), Color.White, GameFont("Menu"), SCORES_LEFT, SPLASH_TOP);
         }
     }
-    //=======================================================
-    //Service provided by Telerik (www.telerik.com)
-    //Conversion powered by Refactoring Essentials.
-    //Twitter: @telerik
-    //Facebook: facebook.com/telerik
-    //=======================================================
+
 }
