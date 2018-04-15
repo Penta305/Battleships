63c74a0732022e5f9c47fcdbd0fc3924effff8c8
Pinepool <pin3pool@gmail.com>
2018-04-15 16:21:05 +1000

    Converted ISeaGrid to CSharp

--- src/model/ISeaGrid.cs
+++ src/model/ISeaGrid.cs
@@ -1,37 +1,55 @@
-/* TODO: FIXME - Elijah
-Was left as VB code - unconvertible
+using System;
 
-''' <summary>
-''' The ISeaGrid defines the read only interface of a Grid. This
-''' allows each player to see and attack their opponents grid.
-''' </summary>
-Public Interface ISeaGrid
+namespace Battleship
+{
+    // The ISeaGrid defines the read only interface of a Grid. This
+    // allows each player to see and attack their opponents grid.
+    public interface ISeaGrid
+    {
+        int Width
+        {
+            get;
+        }
 
-    ReadOnly Property Width() As Integer
+        int Height
+        {
+            get;
+        }
 
-    ReadOnly Property Height() As Integer
+        /*
+        Indicates that the grid has changed.
+        ''' </summary>
+        Event Changed As EventHandler
+         */
+        event EventHandler Changed;
 
-    ''' <summary>
-    ''' Indicates that the grid has changed.
-    ''' </summary>
-    Event Changed As EventHandler
+        /*
+        ''' <summary>
+        ''' Provides access to the given row/column
+        ''' </summary>
+        ''' <param name="row">the row to access</param>
+        ''' <param name="column">the column to access</param>
+        ''' <value>what the player can see at that location</value>
+        ''' <returns>what the player can see at that location</returns>
+        */
 
-    ''' <summary>
-    ''' Provides access to the given row/column
-    ''' </summary>
-    ''' <param name="row">the row to access</param>
-    ''' <param name="column">the column to access</param>
-    ''' <value>what the player can see at that location</value>
-    ''' <returns>what the player can see at that location</returns>
-    ReadOnly Property Item(ByVal row As Integer, ByVal column As Integer) As TileView
+        TileView Item
+        {
+            get;
+        }
 
-    ''' <summary>
-    ''' Mark the indicated tile as shot.
-    ''' </summary>
-    ''' <param name="row">the row of the tile</param>
-    ''' <param name="col">the column of the tile</param>
-    ''' <returns>the result of the attack</returns>
-    Function HitTile(ByVal row As Integer, ByVal col As Integer) As AttackResult
-End Interface
+        // CHECK: Properties don't accept parameters in C#, Change to method?
+        // TileView Item2(int row, int col);
 
-*/
+        /*
+        ''' <summary>
+        ''' Mark the indicated tile as shot.
+        ''' </summary>
+        ''' <param name="row">the row of the tile</param>
+        ''' <param name="col">the column of the tile</param>
+        ''' <returns>the result of the attack</returns>
+        */
+
+        AttackResult HitTile(int row, int col);
+    }
+}
