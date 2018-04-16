7083d81113664420a53b85fd04add7be4b898c75
Penta305 <dylan.janky@gmail.com>
2018-04-16 16:23:23 +1000

    comment fix

--- src/model/ISeaGrid.cs
+++ src/model/ISeaGrid.cs
@@ -16,23 +16,11 @@ namespace Battleship
             get;
         }
 
-        /*
-        Indicates that the grid has changed.
-        ''' </summary>
-        Event Changed As EventHandler
-         */
+        // Indicates that the grid has changed.
         event EventHandler Changed;
 
-        /*
-        ''' <summary>
-        ''' Provides access to the given row/column
-        ''' </summary>
-        ''' <param name="row">the row to access</param>
-        ''' <param name="column">the column to access</param>
-        ''' <value>what the player can see at that location</value>
-        ''' <returns>what the player can see at that location</returns>
-        */
 
+        // Provides access to the given row/column
         TileView Item
         {
             get;
@@ -41,15 +29,7 @@ namespace Battleship
         // CHECK: Properties don't accept parameters in C#, Change to method?
         // TileView Item2(int row, int col);
 
-        /*
-        ''' <summary>
-        ''' Mark the indicated tile as shot.
-        ''' </summary>
-        ''' <param name="row">the row of the tile</param>
-        ''' <param name="col">the column of the tile</param>
-        ''' <returns>the result of the attack</returns>
-        */
-
+        // Mark the indicated tile as shot.
         AttackResult HitTile(int row, int col);
     }
 }
